6be6b8292bed3da75a0877b55ae7d18fd6ef4319
raycho57 <33470855+raycho57@users.noreply.github.com>
2024-12-12 14:58:09 +0900

    Update CameraPreview.java

--- android/src/main/java/com/ultralytics/ultralytics_yolo/CameraPreview.java
+++ android/src/main/java/com/ultralytics/ultralytics_yolo/CameraPreview.java
@@ -62,7 +62,7 @@ public class CameraPreview {
                     .build();
 
             CameraSelector cameraSelector;
-            if (facing == CameraSelector.LENS_FACING_BACK || facing == CameraSelector.LENS_FACING_FRONT) {
+            if (facing == 0 || facing == 1) {
                 cameraSelector = new CameraSelector.Builder()
                         .requireLensFacing(facing)
                         .build();
@@ -79,7 +79,7 @@ public class CameraPreview {
             imageAnalysis.setAnalyzer(Runnable::run, imageProxy -> {
                 // jksong 0705. Operation Toggle Lens to Select Camera Lens, 
                 // No need to consider mirroring
-                predictor.predict(imageProxy, facing == CameraSelector.LENS_FACING_FRONT);
+                predictor.predict(imageProxy, facing == 1);
                 // predictor.predict(imageProxy, false);
 
                 //clear stream for next image
